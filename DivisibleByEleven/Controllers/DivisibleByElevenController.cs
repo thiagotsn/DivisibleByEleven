6e13f7f79fb65dd9f86c8c93ba62eeaee8529074
Thiago Nogueira <tsnapps+github@pm.me>
2020-07-12 22:30:57 +0100

    Initial commit

---
+++ DivisibleByEleven/Controllers/DivisibleByElevenController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IsDivisibleByEleven.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IsDivisibleByEleven.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DivisibleByElevenController : ControllerBase
+    {
+        private readonly ILogger<DivisibleByElevenController> _logger;
+
+        public DivisibleByElevenController(ILogger<DivisibleByElevenController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Response Get([FromBody] DivisibleByEleven numbers)
+        {
+            return numbers.CheckDivisibility();
+        }
+    }
+}
---
+++ DivisibleByEleven/Domain/DivisibleByEleven.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsDivisibleByEleven.Domain
+{
+    public class Response
+    {
+        public IEnumerable<Result> Result { get; set; }
+    }
+
+    public class Result
+    {
+        public string Number { get; set; }
+        public string IsMultiple { get; set; }
+    }
+
+    public class DivisibleByEleven
+    {
+        public IEnumerable<string> Numbers { get; set; }
+
+        internal Response CheckDivisibility()
+        {
+            var result = new List<Result>();
+
+            if (Numbers == null)
+            {
+                return null;
+            }
+
+            foreach (var number in Numbers)
+            {
+                try
+                {
+                    var isMultiple = CheckDivisibility(Math.Abs(Convert.ToInt64(number)));
+                    result.Add(new Result { Number = number, IsMultiple = isMultiple.ToString() });
+                }
+                catch (FormatException)
+                {
+                    result.Add(new Result { Number = number, IsMultiple = "Invalid Number" });
+                }
+                catch (OverflowException)
+                {
+                    result.Add(new Result { Number = number, IsMultiple = "Number Too Big" });
+                }
+                catch (Exception)
+                {
+                    result.Add(new Result { Number = number, IsMultiple = "Unknown Excelption" });
+                }
+            }
+
+            var response = new Response { Result = result };
+
+            return response;
+        }
+
+        internal bool CheckDivisibility(long number)
+        {
+            if (number < 10)
+                return false;
+
+            long part = (number / 10) - (number % 10);
+
+            if (part > 99)
+                return CheckDivisibility(part);
+            else if ((part / 10) == (part % 10))
+                return true;
+
+            return false;
+        }
+    }
+}
